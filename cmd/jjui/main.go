c2cfeb450c524e257ef47a817ddda81c72f699cd
oliverpool <git@olivier.pfad.fr>
2025-12-14 13:16:43 +0100

    ensure deferred functions from main are called

--- cmd/jjui/main.go
+++ cmd/jjui/main.go
@@ -80,22 +80,25 @@ func getJJRootDir(location string) (string, error) {
 }
 
 func main() {
+	os.Exit(run())
+}
+
+func run() int {
 	askpassServer := askpass.NewUnstartedServer("JJUI")
 	if askpassServer.IsSubprocess() {
-		return
+		return 0
 	}
 
 	flag.Parse()
 	switch {
 	case help:
 		flag.Usage()
-		os.Exit(0)
+		return 0
 	case version:
 		fmt.Println(getVersion())
-		os.Exit(0)
+		return 0
 	case editConfig:
-		exitCode := config.Edit()
-		os.Exit(exitCode)
+		return config.Edit()
 	}
 
 	var location string
@@ -108,14 +111,14 @@ func main() {
 		if location, err = os.Getwd(); err != nil {
 			fmt.Fprintf(os.Stderr, "Error: couldn't determine the current directory: %v.\n", err)
 			fmt.Fprintf(os.Stderr, "Please pass the location of a `jj` repo as an argument to `jjui`.\n")
-			os.Exit(1)
+			return 1
 		}
 	}
 
 	rootLocation, err := getJJRootDir(location)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(1)
+		return 1
 	}
 
 	if len(os.Getenv("DEBUG")) > 0 {
@@ -138,23 +141,23 @@ func main() {
 	if output, err := config.LoadConfigFile(); err == nil {
 		if err := config.Current.Load(string(output)); err != nil {
 			fmt.Fprintf(os.Stderr, "Error loading configuration: %v\n", err)
-			os.Exit(1)
+			return 1
 		}
 		if registry, err := context.LoadCustomCommands(string(output)); err != nil {
 			fmt.Fprintf(os.Stderr, "Error loading custom commands: %v\n", err)
-			os.Exit(1)
+			return 1
 		} else {
 			appContext.CustomCommands = registry
 		}
 		if registry, err := context.LoadLeader(string(output)); err != nil {
 			fmt.Fprintf(os.Stderr, "Error loading leader keys: %v\n", err)
-			os.Exit(1)
+			return 1
 		} else {
 			appContext.Leader = registry
 		}
 	} else if !errors.Is(err, fs.ErrNotExist) {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		os.Exit(1)
+		return 1
 	}
 
 	var theme map[string]config.Color
@@ -169,7 +172,7 @@ func main() {
 	theme, err = config.LoadEmbeddedTheme(defaultThemeName)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error loading default theme '%s': %v\n", defaultThemeName, err)
-		os.Exit(1)
+		return 1
 	}
 
 	var userThemeName string
@@ -183,7 +186,7 @@ func main() {
 		theme, err = config.LoadTheme(userThemeName, theme)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error loading user theme '%s': %v\n", userThemeName, err)
-			os.Exit(1)
+			return 1
 		}
 	}
 
@@ -207,7 +210,7 @@ func main() {
 	if config.Current.Ssh.HijackAskpass {
 		if err := askpassServer.StartListening(); err != nil {
 			fmt.Fprintf(os.Stderr, "Error: ssh.hijack_askpass: %v\n", err)
-			os.Exit(1)
+			return 1
 		}
 		defer askpassServer.Close()
 
@@ -218,8 +221,9 @@ func main() {
 	}
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error running program: %v\n", err)
-		os.Exit(1)
+		return 1
 	}
+	return 0
 }
 
 func showPassword(send func(tea.Msg)) func(name, prompt string, done <-chan struct{}) (string, bool) {
