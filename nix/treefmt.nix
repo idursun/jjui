e2f9ea542ce7fc05d8725dc341f500a6b857b541
doprz <52579214+doprz@users.noreply.github.com>
2025-12-17 20:29:42 -0600

    feat(treefmt): load treefmt.toml

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -1,19 +1,12 @@
 {
   perSystem =
     { pkgs, ... }:
+    let
+      tomlPath = ../treefmt.toml;
+      tomlConfig =
+        if builtins.pathExists tomlPath then builtins.fromTOML (builtins.readFile tomlPath) else { };
+    in
     {
-      treefmt = {
-        programs.nixfmt.enable = true;
-        programs.gofmt.enable = true;
-        programs.yamlfmt.enable = true;
-        programs.taplo.enable = true;
-        programs.prettier.enable = true;
-
-        settings.formatter.taplo.excludes = [ "internal/config/default/config.toml" ];
-        settings.formatter.prettier.includes = [
-          "*.md"
-          "*.mdx"
-        ];
-      };
+      treefmt.settings.formatter = tomlConfig.formatter or { };
     };
 }
